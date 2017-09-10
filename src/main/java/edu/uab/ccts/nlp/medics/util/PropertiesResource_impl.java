347e9efb72cec9b7f3c585acbb536819828a2afd
John David Osborne <ozborn@uab.edu>
2017-09-10 17:12:17 -0400

    Make PropertiesResource_impl ExternalResource aware

--- src/main/java/edu/uab/ccts/nlp/medics/util/PropertiesResource_impl.java
+++ src/main/java/edu/uab/ccts/nlp/medics/util/PropertiesResource_impl.java
@@ -5,22 +5,29 @@ import java.io.InputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.apache.uima.fit.component.ExternalResourceAware;
+import org.apache.uima.fit.component.initialize.ConfigurationParameterInitializer;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.ExternalResourceFactory;
 import org.apache.uima.resource.DataResource;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.SharedResourceObject;
 
-public class PropertiesResource_impl implements PropertiesResource, SharedResourceObject {
+public class PropertiesResource_impl implements PropertiesResource, ExternalResourceAware {
   private Properties prop = new Properties();
   private static final Logger log  = LoggerFactory.getLogger(PropertiesResource_impl.class);
 
 
+  @ConfigurationParameter(name=ExternalResourceFactory.PARAM_RESOURCE_NAME)
+  private String resourceName;
+  
   /**
    * Expecting to load property files from jar file, URI should be set to
    * something like: edu/uab/ccts/nlp/database/column_map/CareflowColumnMap.properties
    * @see org.apache.uima.resource.SharedResourceObject#load(DataResource)
    */
   public void load(DataResource aData) throws ResourceInitializationException {
+	ConfigurationParameterInitializer.initialize(this, aData);
     if(aData.getUri()!=null) log.info("Loading properties from URI:"+aData.getUri());
     try (InputStream inStr = ClassLoader.getSystemResourceAsStream(aData.getUri().toString())){
       prop.load(inStr);
@@ -49,4 +56,17 @@ public class PropertiesResource_impl implements PropertiesResource, SharedResour
   public Properties getProperties(){ return prop; }
 
   public void setProperties(Properties p) { prop=p;}
+
+
+@Override
+public String getResourceName() {
+	return resourceName;
+}
+
+
+@Override
+public void afterResourcesInitialized() throws ResourceInitializationException {
+	// TODO Auto-generated method stub
+	
+}
 }
