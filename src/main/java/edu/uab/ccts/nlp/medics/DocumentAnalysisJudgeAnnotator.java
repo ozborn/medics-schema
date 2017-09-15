3ecb8fc74f9a858b16f760be07e42bc70664f58c
John David Osborne <ozborn@uab.edu>
2017-09-15 15:08:49 -0400

    Get MedicsURL from NLP_Analysis

--- src/main/java/edu/uab/ccts/nlp/medics/DocumentAnalysisJudgeAnnotator.java
+++ src/main/java/edu/uab/ccts/nlp/medics/DocumentAnalysisJudgeAnnotator.java
@@ -5,14 +5,12 @@ import java.sql.DriverManager;
 import java.util.Collection;
 import java.util.Iterator;
 
-import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIndex;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Level;
 
 import edu.uab.ccts.nlp.medics.util.LegacyMedicsTools;
@@ -29,18 +27,9 @@ import edu.uab.ccts.nlp.uima.ts.NLP_Clobs;
  */
 public class DocumentAnalysisJudgeAnnotator extends JCasAnnotator_ImplBase {
 
-	private String medicsConnectionString = null;
-	public UimaContext uContext = null;
+	String medicsConnectionString = null;
 	int analysisId = 0;
 	int docsetId = 0;
-
-	public void initialize(UimaContext aContext) throws ResourceInitializationException {
-		super.initialize(aContext);
-		try {
-			uContext = aContext;
-			aContext.getLogger().log(Level.INFO,"Medics Doc/Clob Writing URL is: "+medicsConnectionString+"\n");
-		} catch (Exception e) { throw new ResourceInitializationException(); }
-	}
 
 	@Override
 	public void process(JCas jcas) throws AnalysisEngineProcessException {
@@ -53,6 +42,7 @@ public class DocumentAnalysisJudgeAnnotator extends JCasAnnotator_ImplBase {
 			anal = (NLP_Analysis) analIter.next();
 			analysisId=anal.getAnalysisID();
 			docsetId=anal.getAnalysisDataSet();
+			medicsConnectionString = anal.getMedicsURL();
 			Collection<NLP_Clobs> mdocs = JCasUtil.select(jcas, NLP_Clobs.class);
 			if(mdocs==null || mdocs.size()!=1) {
 				getContext().getLogger().log(Level.SEVERE,"Did not find 1 metadocument!");
@@ -71,7 +61,7 @@ public class DocumentAnalysisJudgeAnnotator extends JCasAnnotator_ImplBase {
 
 
 		} catch (Exception e){
-			uContext.getLogger().log(Level.SEVERE,
+			this.getContext().getLogger().log(Level.SEVERE,
 					"Could not determine the analysis_id , docsetId, or"+
 							" document id. Analysis and docsetID should be set by AnalysisAnnotator (UIMA-FIT) or"
 							+ "OracleTextCollectionReader (Legacy)");
